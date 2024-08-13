6ef7fba46d04d6b625e125bef821a60716963818
Saalu Issaka <saaluissaka@gmail.com>
2024-08-13 23:13:36 +0000

    day2

---
+++ day2/exercise1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generatior.")
+city =input("What city did you grow up in ?\n")
+pet =input("What is your favourite pet ?\n")
+print("Your band name is : " + city + pet)
---
+++ day2/lesson2.py
@@ -0,0 +1,7 @@
+print('Hello!')
+
+username = input("What is your name ?\n ")
+
+print("Hi, my is "  + username)
+
+print("And this is the lenth of my name: ", len(username))
