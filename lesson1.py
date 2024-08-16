853adff1a7e6b44db8889047ebb2414eac7d9a84
Saalu Issaka <saaluissaka@gmail.com>
2024-08-16 01:32:46 +0000

    Day3_continues

--- lesson1.py
+++ lesson1.py
@@ -1,6 +1,15 @@
+# conditionals - if/else, elif
+# conditionals - if/else nested if/else
 print("Rollercosta Event")
 height = int(input("Please enter your height "))
 if height >= 100:
     print("You can ride the rollercosta")
+    age = int(input("What is your age? "))
+    if age <= 12 :
+        print("Please pay $5.")
+    elif age <= 18 :
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
     print("Sorry, you need to grow taller to ride")
---
+++ lesson2.py
@@ -0,0 +1,6 @@
+num_checker = int(input("What is the number you want to check? "))
+
+if num_checker % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
