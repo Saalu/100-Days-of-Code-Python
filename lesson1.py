358bcd53054801773c37c867d944d039605a92b6
Saalu Issaka <saaluissaka@gmail.com>
2024-08-16 01:06:15 +0000

    Day3_Lessons

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+print("Rollercosta Event")
+height = int(input("Please enter your height "))
+if height >= 100:
+    print("You can ride the rollercosta")
+else:
+    print("Sorry, you need to grow taller to ride")
