d0d39ae47166fa98570037aff53213e780045115
Saalu Issaka <saaluissaka@gmail.com>
2024-08-21 21:19:14 +0000

    Complete assignment

--- day5.py/assignment.py
+++ day5.py/assignment.py
@@ -1,6 +1,5 @@
 print("Welcome to Pizza Special\nWe have Small(S)-15 Medium(M)-$20 or Large(L)-$25 ")
 
-
 size = input("What pizza size do you prefer? S, M, or L: ")
 cheese = input("Do you want extra cheese? Y or N: ")
 pepperoni = input("Do you want pepperoni?  Y or N: ")
@@ -19,6 +18,15 @@ elif size == "L":
 else:
     print("You typed a wrong pizza size: S-Small, M-Medium or L-Large ")
 
- 
 
+# Add bill based on pepperoni choice
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+else:
+    bill += 3 
+
+# Add bill based on pepperoni choice
+if cheese == "Y":
+    bill += 1
 
