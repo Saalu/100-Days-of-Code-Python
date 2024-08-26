805c865668f16ab9779e1202a200a437a7241fe4
Saalu Issaka <saaluissaka@gmail.com>
2024-08-26 21:19:21 +0000

    Day_5 third assigment complete

--- assignment3.py
+++ assignment3.py
@@ -1,16 +1,17 @@
+# <<<<<<<<<<<<<<<<<Complete working Game>>>>>>>>>>>>>>>>>>.
 print("Welcome to the Treasure Island")
 print("Your mission is to find the treasure.")
 choice1 = input("Your're at a cross road. Where do you want to go ?\n Type 'left' or 'right' ").lower()
 # choice3 = input("Your've come to a door. There is a Red, Blue and Yellow door.\n Type 'color of door' ")
 
-
 if choice1 == "left":
-    choice2 = input('Your\'ve come to a lake. There is an island in the middle of the lake. '
-                 'Type "wait" to wait for a boat. Type "swim" to swim across ' ).lower()
+    choice2 = input('Your\'ve come to a lake.' 
+                    'There is an island in the middle of the lake.\n'
+                    'Type "wait" to wait for a boat. Type "swim" to swim across: ').lower()
     #Continue....
     if choice2 == "wait":
         choice3 = input('Your\'ve come to a door.'
-                        'There is a Red, Blue and Yellow door.' 
+                        'There is a Red, Blue and Yellow door.\n' 
                         'Type "color of door" ').lower()
         #Continue....
         if choice3 =="yellow":
@@ -21,11 +22,3 @@ if choice1 == "left":
 
 else:
     print("Wrong direction. Game Over")
-
-
-#     if lake == "swim":
-#         print("Game Over")
-#     if color == "blue" or "red":
-#         print("Game Over")
-# else:
-#     print("You win!")
