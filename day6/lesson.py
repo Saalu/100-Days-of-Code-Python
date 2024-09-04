3ae42f0de8c2131eeced80c9c8ddff3e91e6e793
Saalu Issaka <saaluissaka@gmail.com>
2024-09-04 22:01:44 +0000

    Day6_Testing

---
+++ day6/lesson.py
@@ -0,0 +1,18 @@
+def my_func(fname):
+    print(f"Hello from function {fname}")
+
+# my_func("Saalu")
+# my_func("Adamu")
+
+
+def num_func(x):
+    for x in range(1, x):
+        if x < 11:
+            print("Yes")
+        else:
+            print("No")
+
+
+print(num_func(3))
+print(num_func(5))
+print(num_func(9))
