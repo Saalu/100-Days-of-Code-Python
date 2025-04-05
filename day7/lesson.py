ee46aa8e4b61a99ed81b00d36f339e03d8144f42
saalu issaka <saaluissaka@gmail.com>
2025-04-05 21:53:44 +0000

    Day_7 List in python

---
+++ day7/lesson.py
@@ -0,0 +1,28 @@
+fruits = [
+    "Mango",
+    "Pineapple",
+    "Pawpaw",
+    "Banana",
+    "Orange",
+    "Watermelon",
+    "Avocado",
+    "Guava",
+    "Coconut",
+    "Passion Fruit"
+]
+
+vegetables = [
+    "Tomato",
+    "Onion",
+    "Pepper",
+    "Okra",
+    "Eggplant",
+    "Carrot",
+    "Cabbage",
+    "Lettuce",
+    "Spinach",
+    "Cucumber"
+]
+
+print("Fruits:", fruits)
+print("Vegetables:", vegetables)
